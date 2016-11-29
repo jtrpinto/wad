9b48669f6cc6ccafa3c0d82c6de9adf7355bea23
João Ribeiro Pinto <jtrpinto@gmail.com>
2016-11-29 18:17:05 +0000

    SIOBXS

---
+++ files/js/scripts.js
@@ -0,0 +1,19 @@
+function openSideMenu() {
+    document.getElementById("wad-side-menu-opened").style.display = 'inline-block';
+    document.getElementById("wad-side-menu-collapsed").style.display = 'none';
+    document.getElementById("wad-side-menu").style.width = "14em";
+}
+
+function collapseSideMenu() {
+    document.getElementById("wad-side-menu-opened").style.display = 'none';
+    document.getElementById("wad-side-menu-collapsed").style.display = 'inline-block';
+    document.getElementById("wad-side-menu").style.width = "3em";
+}
+
+function openPopUp(id){
+  document.getElementById(id).style.visibility = 'visible';
+}
+
+function closePopUp(id){
+  document.getElementById(id).style.visibility = 'hidden';
+}
