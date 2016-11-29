47ddac3876166be3da1135b7462dfba0f97a558e
João Ribeiro Pinto <jtrpinto@gmail.com>
2016-11-29 19:02:10 +0000

    Passing index to php

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
-<!DOCTYPE HTML>
+<?php
+$conn = new PDO('pgsql:host=dbm.fe.up.pt;port=5432;dbname=tweb1601','tweb1601','Monet1840');
+$conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+$conn->query("SET SCHEMA 'public'");
+?>
+
 <html>
 <head>
   <title>WAD</title>
