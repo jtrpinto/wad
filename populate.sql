e6b57b77c7df79dcba698f7b21696a3a6ce72e03
João Ribeiro Pinto <jtrpinto@gmail.com>
2016-12-12 18:21:10 +0000

    More database population

--- populate.sql
+++ populate.sql
@@ -4,26 +4,23 @@ VALUES ('Osteoporosis','');
 INSERT INTO doctors (first_name, last_name, birth_date, gender, phone, citizen_id, email,
   speciality_id, first_day_of_service, is_active, password, username,
   country, city, street, floor_app, doornumber, postal_code)
-VALUES ('Elliot', 'Reid', '1981-03-12', 'F', '445559869', '10998099', 'e_reid@wad.us', '1', '2005-09-20', '1',
-  'EREID', 'elliotreid', 'USA', 'San DiFrangeles, CA', 'Fox Ave.', '2A',
-  '233', '23409');
+VALUES ('Elliot', 'Reid', '1981-03-12', 'f', '445559869', '10998099', 'e_reid@wad.us', '1', '2005-09-20', '1',
+  'EREID', 'elliotreid', 'USA', 'San DiFrangeles, CA', 'Fox Ave.', '2A', '233', '23409');
 
+INSERT INTO doctors (first_name, last_name, birth_date, gender, phone, citizen_id, email,
+  speciality_id, first_day_of_service, is_active, password, username,
+  country, city, street, floor_app, doornumber, postal_code)
+VALUES ('John', 'Dorian', '1981-05-03', 'm', '445550021', '10975589', 'jd_md@wad.us', '1', '2005-09-19', '1',
+    'JDandTURK4EVA', 'jdmd', 'USA', 'San DiFrangeles, CA', 'Kent Blvd.', '6C', '1022', '23404');
 
-first_name	character varying(128)
-last_name	character varying(128)
-birth_date	date
-gender	character(1)
-phone	character varying(9)
-citizen_id	character varying(128)
-email	character varying
-speciality_id	integer
-first_day_of_service	date
-is_active	boolean
-password	character varying(128)
-username	character varying(128)
-country	character varying(128)
-city	character varying(128)
-street	character varying(128)
-floor_app	character varying(128)
-doornumber character varying(128)
-postal_code character varying(128)
+INSERT INTO doctors (first_name, last_name, birth_date, gender, phone, citizen_id, email,
+  speciality_id, first_day_of_service, is_active, password, username,
+  country, city, street, floor_app, doornumber, postal_code)
+VALUES ('Robert', 'Kelso', '1964-09-21', 'm', '445550903', '10099008', 'kelso@wad.us', '1', '1988-07-04', '1',
+  'backToWork', 'drKelso', 'USA', 'Anaheim, CA', 'Central Ave.', '3', '2305', '21300');
+
+INSERT INTO doctors (first_name, last_name, birth_date, gender, phone, citizen_id, email,
+  speciality_id, first_day_of_service, is_active, password, username,
+  country, city, street, floor_app, doornumber, postal_code)
+VALUES ('Christopher', 'Turk', '1980-09-11', 'm', '445550877', '11109938', 'turky@wad.us', '1', '2005-09-20', '1',
+    'MostGiantDoctor', 'turky', 'USA', 'San DiFrangeles, CA', 'Kent Blvd.', '6C', '1022', '23404');
