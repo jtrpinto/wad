e04a8d50bb152e09ddde07fc73fbf2669c1cd81c
PatriciaLR <patriciarodrigues.94@live.com.pt>
2017-01-02 21:33:01 +0000

    version4.1

---
+++ editPatientInfo.php
@@ -0,0 +1,22 @@
+<?php
+include('templates/header.php');
+$_GET['class1'] = "";
+$_GET['class2'] = "wad-side-menu-button-active";
+$_GET['class3'] = "";
+$_GET['class4'] = "";
+include('templates/body.php');
+
+$_GET['classMenu1'] = "button-submit-pat-active";
+$_GET['classMenu2'] = "";
+$_GET['classMenu3'] = "";
+$_GET['classMenu4'] = "";
+include('patientPage.php');
+?>
+
+
+
+</div>
+
+<?php
+include('templates/footer.php');
+?>
