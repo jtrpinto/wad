ccf6a860d4aed7dea79d5b31074e1ab08a02c367
João Ribeiro Pinto <jtrpinto@gmail.com>
2017-01-08 15:22:50 +0000

    Fixing login to allow only active doctors

--- site/0_action_login.php
+++ site/0_action_login.php
@@ -2,27 +2,25 @@
 include ('config/init.php');
 
   $username = trim(strip_tags($_POST['username']));
-  $password = $_POST['password'];  
+  $password = $_POST['password'];
 
   global $conn;
-  $stmt = $conn->prepare('SELECT * FROM wad.doctors WHERE username = ? AND password = ?');
+  $stmt = $conn->prepare('SELECT * FROM wad.doctors WHERE username = ? AND password = ? AND is_active = TRUE');
   $stmt->execute(array($username, sha1($password))); //
   $user = $stmt->fetch();
 
   if (print_r($user['id'], true) !== '') {
-    
+
     $doctor_id = print_r($user['id'], true);
     $_SESSION['doctor_id'] = $doctor_id;
     $_SESSION['success_message'] = "Login succesful!";
 
     //file_put_contents('abc.txt', print_r($_SESSION['doctor_id'], true));
-    header ('Location: index.php'); 
-  } 
+    header ('Location: index.php');
+  }
   else {
     $_SESSION['error_message'] = "Login failed!";
     header ('Location: myLogin.php');
   }
 
 ?>
-
-
